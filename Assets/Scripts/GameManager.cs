16f40594729e4716cb91ea3a0f6713e2a27fbe7a
huanghuishan <609500017@qq.com>
2024-03-26 15:11:35 +0800

    首次提交

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour {
+
+	public TileBoard board;
+	public CanvasGroup gameOver;
+	public TextMeshProUGUI scoreText;
+	public TextMeshProUGUI highScoreText;
+
+	private int score;
+
+	private void Start () {
+		NewGame();
+	}
+
+	public void NewGame() {
+		SetScore(0);
+		highScoreText.text = LoadHighScore().ToString();
+
+		gameOver.alpha = 0f;
+		gameOver.interactable = false;
+
+		board.ClearBoard();
+		board.CreateTile();
+		board.CreateTile();
+		board.enabled = true;
+	}
+
+	public void GameOver() {
+		board.enabled = false;
+		gameOver.interactable = true;
+
+		// 第二个参数：0出，1入
+		StartCoroutine(Fade(gameOver, 1f, 1f));
+	}
+
+	private IEnumerator Fade(CanvasGroup canvasGroup, float to, float delay) {
+		yield return new WaitForSeconds(delay);
+
+		float elapsed = 0f;
+		float duration = .5f;
+		float from = canvasGroup.alpha;
+
+		while (elapsed < duration) {
+			canvasGroup.alpha = Mathf.Lerp(from, to, elapsed / duration);
+			elapsed += Time.deltaTime;
+			yield return null;
+		}
+
+		canvasGroup.alpha = to;
+	}
+
+	public void IncreaseScore(int points) {
+		SetScore(score + points);
+	}
+
+	private void SetScore(int score) {
+		this.score = score;
+
+		scoreText.text = score.ToString();
+
+		SaveHighScore();
+	}
+
+	private void SaveHighScore() {
+		int highScore = LoadHighScore();
+
+		if(score > highScore) {
+			PlayerPrefs.SetInt("highScore", score);
+		}
+	}
+
+	private int LoadHighScore() {
+		return PlayerPrefs.GetInt("highScore", 0);
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour {
+    public TileState state {  get; private set; }
+    public TileCell cell { get; private set; }
+    public int number { get; private set; }
+    public bool locked { get; set; }
+
+    private Image background;
+    private TextMeshProUGUI text;
+
+    private void Awake() {
+        background = GetComponent<Image>();
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void SetState(TileState state, int number) {
+        this.state = state;
+        this.number = number;
+
+        background.color = state.backgroundColor;
+        text.color = state.textColor;
+        text.text = number.ToString();
+    }
+
+    public void Spawn(TileCell cell) {
+        if (this.cell != null) {
+            this.cell.tile = null;
+        }
+
+        this.cell = cell;
+        this.cell.tile = this;
+
+        transform.position = cell.transform.position;
+    }
+
+    public void MoveTo(TileCell cell) {
+        if (this.cell != null) {
+            this.cell.tile = null;
+        }
+
+        this.cell = cell;
+        this.cell.tile = this;
+
+        StartCoroutine(Animate(cell.transform.position, false));
+    }
+
+    public void Merge(TileCell cell) {
+        if (this.cell != null) {
+            this.cell.tile = null;
+        }
+
+        this.cell = null;
+        cell.tile.locked = true;
+
+        StartCoroutine(Animate(cell.transform.position, true));
+    }
+
+    /// <summary>
+    /// A到B的动画
+    /// </summary>
+    /// <param name="to">目的地</param>
+    /// <returns></returns>
+    private IEnumerator Animate(Vector3 to, bool merging) {
+        float elapsed = 0f;
+        float duration = 0.1f;
+
+        Vector3 from = transform.position;
+
+        while (elapsed < duration) {
+            // 从A到B
+            // 最后一个参数：百分比，即经过的时间 / 持续时间
+            transform.position = Vector3.Lerp(from, to, elapsed / duration);
+            elapsed += Time.deltaTime;// 一帧的时间
+            yield return null;// 在此种情况下暂停执行该函数直到下一帧
+        }
+
+        transform.position = to;
+
+        if (merging) {
+            Destroy(gameObject);
+        }
+    }
+}
